f977e475e471a3e79990cc2c79e080bdc3e588ae
toninoes <toninoes@gmail.com>
2025-01-25 19:07:31 +0100

    Add IPs outputs for VM

--- azure/virtual_machine/outputs.tf
+++ azure/virtual_machine/outputs.tf
@@ -1,1 +1,9 @@
-# Our Outputs here
+output "private_ip" {
+  description = "Private IP of virtual machine"
+  value = azurerm_network_interface.this.private_ip_address
+}
+
+output "public_ip" {
+  description = "Public IP of virtual machine"
+  value = var.enable_public_ip ? azurerm_public_ip.this[0].ip_address : null
+}
