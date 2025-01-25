33664f06d06dd9fffede162bf786e955cd0f2706
toninoes <toninoes@gmail.com>
2025-01-25 13:56:03 +0100

    Change subnet names

--- azure/virtual_network/main.tf
+++ azure/virtual_network/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_subnet" "this" {
   for_each = { for subnet in var.subnets : subnet.name => subnet }
 
   address_prefixes     = each.value.address
-  name                 = each.value.name
+  name                 = "${var.virtual_network_name}-${each.value.name}"
   resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
 }
