ebc1e5814eedcb94b7c26efb2db57cd43dc0302a
toninoes <toninoes@gmail.com>
2025-02-01 20:43:27 +0100

    fix typo

--- azure/virtual_network/main.tf
+++ azure/virtual_network/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_public_ip" "bastion" {
   location            = data.azurerm_resource_group.this.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
-  sku                 = "Stardard"
+  sku                 = "Standard"
 }
 
 resource "azurerm_bastion_host" "this" {
