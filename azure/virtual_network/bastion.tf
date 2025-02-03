3fa01d54154b1b42a37ee832ba331a3a0342f392
toninoes <toninoes@gmail.com>
2025-02-03 12:49:32 +0100

    Refactor

---
+++ azure/virtual_network/bastion.tf
@@ -0,0 +1,32 @@
+resource "azurerm_subnet" "bastion" {
+  count = (length(var.subnet_addresses_for_bastion) == 0) ? 0 : 1
+
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = var.subnet_addresses_for_bastion
+}
+
+resource "azurerm_public_ip" "bastion" {
+  count = (length(var.subnet_addresses_for_bastion) == 0) ? 0 : 1
+
+  name                = "${var.virtual_network_name}-bastion-ip"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "this" {
+  count = (length(var.subnet_addresses_for_bastion) == 0) ? 0 : 1
+
+  name                = "${var.virtual_network_name}-bastion"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion[0].id
+    public_ip_address_id = azurerm_public_ip.bastion[0].id
+  }
+}
