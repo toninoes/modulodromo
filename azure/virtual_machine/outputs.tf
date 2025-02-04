76e687ca20f67c1b97d9f0c9bece585176f746e3
toninoes <toninoes@gmail.com>
2025-02-04 17:45:00 +0100

    Change in outputs

--- azure/virtual_machine/outputs.tf
+++ azure/virtual_machine/outputs.tf
@@ -1,24 +1,26 @@
 output "private_ip" {
-  description = "Private IP of virtual machine"
+  description = "Private IP of virtual machine."
   value       = azurerm_network_interface.this.private_ip_address
 }
 
 output "public_ip" {
-  description = "Public IP of virtual machine"
+  description = "Public IP of virtual machine."
   value       = one(azurerm_public_ip.this[*].ip_address)
 
   depends_on = [azurerm_linux_virtual_machine.this, azurerm_windows_virtual_machine.this]
 }
 
 output "nsg_name" {
-  description = "Network Security Group Name"
+  description = "Network Security Group Name."
   value       = azurerm_network_security_group.this.name
 }
 
 output "ssh_key_private_name" {
+  description = "Filename for private key file."
   value = one(local_file.pem[*].filename)
 }
 
 output "ssh_key_public_name" {
+  description = "Filename for public key file."
   value = one(local_file.pub[*].filename)
 }
