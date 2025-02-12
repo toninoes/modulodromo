f1715f10a92c95b53510a091e560cc38aa13511d
toninoes <toninoes@gmail.com>
2025-02-12 21:27:15 +0100

    Added output.

---
+++ azure/app_service_containerized/outputs.tf
@@ -0,0 +1,19 @@
+output "container_registry_login_server" {
+  description = "The URL used to log into the container registry."
+  value       = azurerm_container_registry.this.login_server
+}
+
+output "container_registry_admin_username" {
+  description = "The Username associated with the Container Registry Admin account - if the admin account is enabled."
+  value       = azurerm_container_registry.this.admin_username
+}
+
+output "container_registry_admin_password" {
+  description = "The Password associated with the Container Registry Admin account - if the admin account is enabled."
+  value       = azurerm_container_registry.this.admin_password
+}
+
+output "web_app_name" {
+  description = "Web App name."
+  value       = azurerm_linux_web_app.this.name
+}
